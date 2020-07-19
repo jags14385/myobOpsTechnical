cce831b90b7ca15f4330332efaac0102bd00dde7
Jags <jags14385@users.noreply.github.com>
2020-07-19 23:38:08 +1000

    Got the Hello Lambda deployed.

--- lib/myob_ops_technical-stack.ts
+++ lib/myob_ops_technical-stack.ts
@@ -1,18 +1,15 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
 
 export class MyobOpsTechnicalStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'MyobOpsTechnicalQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const helloLambda = new lambda.Function(this,"HelloLambda",{
+      code: lambda.Code.asset("lambda"),
+      handler: "hello.handler",
+      runtime:lambda.Runtime.NODEJS_12_X,
     });
-
-    const topic = new sns.Topic(this, 'MyobOpsTechnicalTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
+  
 }
